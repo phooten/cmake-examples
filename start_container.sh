2491195678ff92713ba2c65a79505ec1c80a02d6
Parker Hooten <parkerhooten@gmail.com>
2026-05-02 14:53:33 -0600

    Startup script to mount folders for docker

--- start_container.sh
+++ start_container.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 docker run -v ~/.bashrc:/home/devuser/.bashrc -v /Users/phoot/code/gitlab/classes/github_cmake_examples-cmake:/home/devuser/code -it matrim/cmake-examples:3.5.1
-cd ~/code
