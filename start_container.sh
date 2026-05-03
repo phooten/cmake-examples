1a44214d916965d2d760567fe39a209f929e8455
Parker Hooten <parkerhooten@gmail.com>
2026-05-02 21:24:52 -0600

    adding newer version of CMake

--- start_container.sh
+++ start_container.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -v ~/.bashrc:/home/devuser/.bashrc -v /Users/phoot/code/gitlab/classes/github_cmake_examples-cmake:/home/devuser/code -it matrim/cmake-examples:3.5.1
+docker run \
+    -v ~/.bashrc:/home/devuser/.bashrc \
+    -v /Users/phoot/code/gitlab/classes/github_cmake_examples-cmake:/home/devuser/code \
+    -it cmake-examples:3.26.6
+    # -it matrim/cmake-examples:3.5.1
