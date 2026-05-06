65346be314a358393301fd904539341a03c04e39
Parker Hooten <parkerhooten@gmail.com>
2026-05-05 22:35:04 -0600

    Completed 01/F exercise

--- dockerfiles/start_container.sh
+++ dockerfiles/start_container.sh
@@ -2,5 +2,6 @@
 docker run \
     -v ~/.bashrc:/home/devuser/.bashrc \
     -v /Users/phoot/code/gitlab/classes/github_cmake_examples-cmake:/home/devuser/code \
-    -it cmake-examples:3.26.6
+    -it hooten:cmake
+    # -it cmake-examples:3.26.6
     # -it matrim/cmake-examples:3.5.1
